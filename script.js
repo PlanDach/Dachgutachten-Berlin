208a3d6ef3c002fd28d4d175564d3b63faed8505
PlanDach <philipp.hoefinghoff@live.de>
2025-09-08 18:38:38 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Navigation - Hamburger Menü
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(".hamburger");
+  const navLinks = document.querySelector(".nav-links");
+
+  hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+});
