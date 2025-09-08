53422ef89b9560cd39583cc25fc038ef48bde17b
PlanDach <philipp.hoefinghoff@live.de>
2025-09-08 23:19:24 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+// Hamburger-Menü
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('navMenu');
+
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
+
+// Submenu mobil
+const submenuToggles = document.querySelectorAll('.submenu-toggle');
+submenuToggles.forEach(toggle => {
+  toggle.addEventListener('click', (e) => {
+    e.preventDefault();
+    const submenu = toggle.nextElementSibling;
+    submenu.classList.toggle('active');
+  });
+});
